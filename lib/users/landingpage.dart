77e41d3a017626ed3141d0ef50853047a2527840
jihanazz <jihanazzahra.2019@student.uny.ac.id>
2022-01-16 20:13:20 +0700

    first commit

---
+++ lib/users/landingpage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget {
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('YukBli'),
+      ),
+      body: Center(
+        child: Text(
+          'Launcher Page',
+        ),
+      ),
+    );
+  }
+}
